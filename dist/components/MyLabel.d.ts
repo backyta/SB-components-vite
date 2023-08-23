256f903f24901ba6ddbcf37c7677c6c97711b8a8
Kevin Baca Angeles <kevi.baca18@gmail.com>
2023-08-23 15:16:19 -0500

    feat: added sematic-versioning

---
+++ dist/components/MyLabel.d.ts
@@ -0,0 +1,28 @@
+import './MyLabel.css';
+export interface Props {
+    /**
+     * Text to display
+    */
+    label: string;
+    /**
+     * Label size
+    */
+    size?: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * Capitalizr all letters
+    */
+    AllCaps?: boolean;
+    /**
+     * Label color
+    */
+    color?: 'text-primary' | 'text-secondary' | 'text-tetiary';
+    /**
+     * Font Color
+    */
+    fontColor?: string;
+    /**
+     * background Color
+    */
+    backgroundColor?: string;
+}
+export declare const MyLabel: ({ label, size, AllCaps, color, fontColor, backgroundColor }: Props) => import("react/jsx-runtime").JSX.Element;
---
+++ dist/components/MyLabel.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import './MyLabel.css';
+export const MyLabel = ({ label, size = 'normal', AllCaps = false, color = 'text-primary', fontColor, backgroundColor = 'transparent' }) => {
+    return (_jsx("span", { className: `${size} ${color} label`, style: { color: fontColor, borderRadius: '1rem', padding: '0.5rem', backgroundColor }, children: AllCaps ? label.toUpperCase() : label }));
+};
